352758675d0d104a2ff8a230f4e4b578d5e4d576
Jake Sendar <jakesendar@gmail.com>
2015-03-19 21:32:19 -0400

    adds calendar api functionality

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,12 +1,36 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+    , router = express.Router()
+    , request = require('request');
+
+var baseURI = 'https://www.googleapis.com/calendar/v3' 
 
 router.get('/', function(req, res) {
-    res.send('Im a calendar ' + req.query.accessToken);
-})
+    // specify the fields we want:
+    var fields ='items(id,summary,colorId,selected,timeZone)';
+    var accessToken = req.query.accessToken;
+    var uri = baseURI + '/users/me/calendarList?fields=' + 
+        fields + '&access_token=' + accessToken;
 
-router.get('/:id', function(req, res) {
-    res.send('The id is' + req.params.id);
-})
+    request.get(uri, function(err, r, body) { 
+        var items = JSON.parse(body).items;
+        res.json(items);
+    });
+});
+
+router.get('/:id/events', function(req, res) {
+    var fields = 'items(status,locked,organizer(displayName,email,self),' + 
+        'recurrence,attendees(displayName,email,responseStatus,self),' + 
+        'summary,location,start,end,id)';
+    var accessToken = req.query.accessToken;
+    var id = req.params.id;
+    var uri = baseURI + '/calendars/' + id + 
+        '/events?fields=' + fields + 
+        '&access_token=' + accessToken;
+
+    request.get(uri, function(err, r, body) {
+        var items = JSON.parse(body).items;
+        res.json(items)
+    });
+});
 
 module.exports = router;
