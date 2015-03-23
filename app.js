b41878cda109bee4e5545611b7894d2ed65a92d1
Jake Sendar <jakesendar@gmail.com>
2015-03-23 12:50:31 -0400

    removes session

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var express = require('express')
     , app = express()
-    , session = require('express-session')
     , calendarRouter = require('./routes/calendar')
     , authenticateRouter = require('./routes/authenticate');
 
-app.use(session({secret: 'keyboard cat'}));
 app.use('/calendars', calendarRouter);
 app.use('/authenticate', authenticateRouter);
 
