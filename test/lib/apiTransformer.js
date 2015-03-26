55a89250961d4a1eff9dd9f54ceae66be8e7cfaf
Jake Sendar <jakesendar@gmail.com>
2015-03-26 13:07:16 -0400

    adds apiTransformer tests

--- test/lib/apiTransformer.js
+++ test/lib/apiTransformer.js
@@ -1,31 +1,47 @@
+var should = require('should')
+    , ApiTransformer = require('./../../lib/apiTransformer');
 
-describe("#transformItems()", function() {
-    var apiRequest = new ApiRequest(DEFAULT_OPTS);
+describe("ApiTransformer", function() {
 
-    it("should transform an array of items", function() {
-        var items = [{"summary": "foo"}]
-        apiRequest.transformItems(items).should.eql([{"title": "foo"}]);
+    describe("#new()", function() {
+        context("without opts", function() {
+            var apiTransformer = new ApiTransformer({});
+
+            it("should have a keysMap property", function() {
+                apiTransformer.should.have.property("keysMap");
+            });
+        });
+
+        context("with keysMap opt", function() {
+            var apiTransformer = new ApiTransformer({keysMap: true});
+
+            it("should have a custom keysMap property", function() {
+                apiTransformer.should.have.property("keysMap", true);
+            });
+        });
+
     });
 
-    it("should handle nested transforms", function() {
-        var items = [{"attendees": {"displayName": "foo"}}]
-        apiRequest.transformItems(items).should.eql([{"attendees": {"name": "foo"}}]);
-    });
+    describe("#transformItemKeys()", function() {
+        var apiTransformer = new ApiTransformer({});
 
-    it("should do nothing to unspecified keys", function() {
-        var items = [{"foo": "bar"}]
-        apiRequest.transformItems(items).should.eql([{"foo": "bar"}]);
-    });
+        it("should transform an array of items", function() {
+            var items = [{"summary": "foo"}]
+            apiTransformer.transformItemKeys(items)
+            .should.eql([{"title": "foo"}]);
+        });
 
+        it("should handle nested transforms", function() {
+            var items = [{"attendees": {"displayName": "foo"}}];
+            apiTransformer.transformItemKeys(items)
+            .should.eql([{"attendees": {"name": "foo"}}]);
+        });
+
+        it("should do nothing to unspecified keys", function() {
+            var items = [{"foo": "bar"}];
+            apiTransformer.transformItemKeys(items)
+            .should.eql([{"foo": "bar"}]);
+        });
+    });
 });
 
-it("should perform custom key transforms", function(done) {
-    var opts = DEFAULT_OPTS;
-    opts.keysMap = {"oldKey": "newKey"};
-    var apiRequest = new ApiRequest(opts);
-    API.reply(200, {"items": [{"oldKey": "bar"}]});
-    apiRequest.requestItems(true, function(err, data) {
-        data.should.eql([{newKey: "bar"}]);
-        done();
-    });
-});
