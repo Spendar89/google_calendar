ad7883415ec3aa862a8831c7258660b30d837c9e
Jake Sendar <jakesendar@gmail.com>
2016-05-15 02:30:28 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ var Config = {
     tokenUri: 'https://accounts.google.com/o/oauth2/token',
     scope: 'https://www.googleapis.com/auth/calendar',
     redirectUri: 'http://localhost:8000/authenticate/callback',
-    clientId: '769981222617-71a6h6aated5cc15k6ss8sdqfrsndm3r.apps.googleusercontent.com',
-    clientSecret: '9pAII4nYg6zVSUePgVHqm3Ge'
+    clientId: '',
+    clientSecret: ''
 }
 
 module.exports = Config;
